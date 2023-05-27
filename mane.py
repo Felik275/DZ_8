d670d28c55052a0d19559a37195755b2e036cb75
Felik275 <bender32010@gmail.com>
2023-05-27 13:46:34 +0300

    Last mane_function adjsustment 27/05/2023 13:46

--- mane.py
+++ mane.py
@@ -3,5 +3,6 @@ def mane_function():
     print('Hello user!')
     print('Today -', datetime.datetime.today().date())
     print('Time -', datetime.datetime.today().time())
+    print('Datetime -', datetime.datetime.today())
 
 mane_function()
