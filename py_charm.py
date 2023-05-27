6ef94ca8774695291c79a8295471f6b7bd0cd2c4
Felik275 <bender32010@gmail.com>
2023-05-27 13:56:40 +0300

    Commited via Pycharm Interface "Adding a timestamp in function py_charm func" 27.05.2023 13:56

--- py_charm.py
+++ py_charm.py
@@ -1,6 +1,8 @@
-
+import datetime
 def py_charm_func():
     print('This is the first test')
     print('This the second line text')
+    print('Current timestamp -', datetime.datetime.now())
+
 
 py_charm_func()
