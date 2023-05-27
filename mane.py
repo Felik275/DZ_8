b22e03ec20e26f1404e2906bb08fdd26d1364b31
Felik275 <bender32010@gmail.com>
2023-05-27 13:42:49 +0300

    Final version of mane_function 27/05/2023 13:44

--- mane.py
+++ mane.py
@@ -2,6 +2,6 @@ import datetime
 def mane_function():
     print('Hello user!')
     print('Today -', datetime.datetime.today().date())
-
+    print('Time -', datetime.datetime.today().time())
 
 mane_function()
